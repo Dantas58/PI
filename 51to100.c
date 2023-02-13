e8cdeb0f68f322647b28fd44f5d5b7841680e387
Phy <pedrodantas8@gmail.com>
2023-02-13 23:03:27 +0000

    Ate p77

--- 51to100.c
+++ 51to100.c
@@ -220,3 +220,263 @@ int removeDups (LInt *l){
 
 
 // 62
+int removeMaiorL (LInt *l){
+
+    if(!l) return -1;
+
+    int max = 0;
+    LInt copial = *l;
+    LInt ant = NULL;
+
+    for(; copial; copial=copial->prox) if(copial->valor > max) max = copial->valor;
+    copial = *l;
+    for(; copial; copial=copial->prox){
+        if(copial->valor == max){
+            if(!ant){*l=(*l)->prox; free(copial); return max;}
+            else{ant->prox = copial->prox; free(copial); return max;}
+        }
+        ant = copial;
+    }
+}
+
+
+// 63
+void init (LInt *l){
+
+    LInt copial = *l; LInt ant = NULL;
+    
+    for(; copial->prox; copial=copial->prox) ant = copial;
+    free(copial);
+
+    if(!ant) *l = NULL;
+    else ant->prox = NULL;
+}
+
+
+// 64
+void appendL (LInt *l, int x){  
+
+    LInt node = malloc(sizeof(LInt));
+    node->valor = x;
+    node->prox = NULL;
+
+    if(!*l){*l = node; return;}
+    LInt copial = *l;
+
+    for(; copial->prox; copial=copial->prox);
+    copial->prox = node;
+}
+
+
+// 65
+void concatL (LInt *a, LInt b){
+
+    if(!*a){*a = b; return;}
+
+    LInt copiaa = *a;
+
+    for(; copiaa->prox; copiaa=copiaa->prox);
+    copiaa->prox = b;
+}
+
+
+// 66
+LInt cloneL (LInt l){   // confuso mas acho que funfa
+
+    if(!l) return NULL;
+
+    LInt copial = l->prox;
+    LInt novo = malloc(sizeof(LInt));
+    novo->valor = l->valor;
+    novo->prox = NULL;
+    LInt r = novo;
+
+    for(; copial; copial=copial->prox){
+
+        LInt prox = malloc(sizeof(LInt));
+        prox->valor = copial->valor;
+        prox->prox = NULL;
+        novo->prox = prox;
+        novo = novo->prox;
+    }   
+
+    return r;
+}
+
+
+// 67 
+LInt cloneRev (LInt l){     // para depois
+
+}
+
+
+// 68
+int maximo (LInt l){
+
+    int max = l->valor;
+    LInt copial = l->prox;
+
+    for(; copial; copial=copial->prox) if(copial->valor > max) max = copial->valor;
+
+    return max;
+}
+
+
+// 69
+int take (int n, LInt *l){
+
+    int N = n;
+    LInt copial = *l; 
+
+    for(N; N>1 && copial; N--) copial = copial->prox;
+
+    if(!copial) return n-N;
+
+    LInt aux = copial->prox;
+    for(; aux; aux=aux->prox) free(aux);
+    copial->prox = NULL;
+
+    return n;
+}
+
+
+// 70
+int drop (int n, LInt *l){
+
+    if(!*l || n==0) return 0;
+
+    int N = n;
+    LInt ant = NULL;
+
+    for(N; N>0 && *l; N--){
+
+        ant = *l;
+        *l = (*l)->prox;
+        free(ant);
+    }
+
+    if(N) return n-N;
+
+    return n;
+}
+
+
+// 71
+LInt Nforward (LInt l, int N){
+
+    if(!l) return NULL;
+
+    LInt copial = l;
+    int n;
+
+    for(n=N; n>0; n--) copial = copial->prox;
+
+    return copial;
+}
+
+
+// 72
+int listToArray (LInt l, int v[], int N){
+
+    int i;
+    LInt copial = l;
+
+    for(i=0; i<N && copial; i++){
+        v[i] = copial->valor;
+        copial=copial->prox;
+    }
+    return i;
+}
+
+
+// 73
+LInt arrayToList (int v[], int N){
+
+    if(!N) return NULL;
+
+    int i;
+    LInt l = malloc(sizeof(LInt));
+    l->valor = v[0];
+    l->prox = NULL;
+
+    LInt aux = l;
+
+    for(i=1; i<N; i++){
+        aux->prox = malloc(sizeof(LInt));
+        aux = aux->prox;
+        aux->valor = v[i];
+        aux->prox = NULL;
+    }
+    return l;
+}
+
+
+// 74
+LInt somasAcL (LInt l){     // codeboard da erro mas acho que é isto
+
+    if(!l) return l;
+
+    int i = 0;
+    LInt copial = l;
+
+    for(; copial; copial=copial->prox){
+
+        i += copial->valor;
+        copial->valor = i;
+    }
+    return l;
+}
+
+
+// 75
+void remreps (LInt l){
+
+    if (!l) return;
+
+    LInt copial = l;
+    LInt aux = l->prox;
+
+    while (aux){
+        if (copial->valor == aux->valor){
+            copial->prox = aux->prox;
+            free(aux);
+            aux = copial->prox;
+        } 
+        
+        else {
+            copial = aux;
+            aux = aux->prox;
+        }
+    }
+}
+
+
+// 76   
+LInt rotateL (LInt l){
+
+    if(!l || !l->prox) return l;
+
+    LInt copial = l;
+    LInt aux = l;
+    
+    LInt node = malloc(sizeof(LInt));
+    node->valor = l->valor;
+    node->prox = NULL;
+
+
+    for(; copial->prox; copial=copial->prox);
+    
+    copial->prox = node;
+    //free(l)  big no no
+    l = l->prox;
+    free(aux);
+    
+    return l;
+}
+
+
+// 77
+LInt parte (LInt l){
+
+    
+}
